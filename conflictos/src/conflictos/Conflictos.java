15086ec2a294f20076f76c71e5a7a99e5234979e
Usuario <Usuario@Equipo-4-Z2-20>
2017-05-05 12:25:14 +0200

    Primer Commit

---
+++ conflictos/src/conflictos/Conflictos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conflictos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Conflictos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Escrito por Rubén");
+    }
+    
+}
